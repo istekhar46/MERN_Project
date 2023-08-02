8acbcbf36474cc925530e240df640bbb9ae6c392
istekhar khan <istekharkhan46@gmail.com>
2023-08-02 23:24:29 +0530

    commit for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import express, { json } from "express";
+import path from 'path'
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -16,9 +17,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.get('/', (req, res) => {
-    res.send("Hello server!!")
-})
+
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+    app.get('*', (req, res) => res.sendFile((path.resolve(__dirname, 'frontend', 'dist', 'index.html'))));
+} else {
+    app.get('/', (req, res) => {
+        res.send("Hello server!!")
+    })
+}
 
 app.use('/api/users', userRoutes);
 app.use('/api/blogs', blogRoutes);
