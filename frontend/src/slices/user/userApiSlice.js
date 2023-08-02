04f5b45aba3c1fc20120fed3fd2ce16f7590efb6
istekhar khan <istekharkhan46@gmail.com>
2023-08-03 02:05:28 +0530

    update for deploy

--- frontend/src/slices/user/userApiSlice.js
+++ frontend/src/slices/user/userApiSlice.js
@@ -1,6 +1,6 @@
 import { apiSlice } from "../apiSlice";
 
-const USERS_URL = '/api/users';
+const USERS_URL = 'https://blogsite-fpfc.onrender.com/api/users';
 
 export const userApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
